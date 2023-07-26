7cd51caaaf32f59b3ca13e26378739cbdb82b9f1
Juan David Fuentes Caceres <juan.fuentes1111@misena.edu.co>
2023-07-25 19:49:03 -0500

    Beta 1.0

--- models/server.js
+++ models/server.js
@@ -20,7 +20,7 @@ class Server {
   middlewares() {
     this.app.use(express.json());
     this.app.use(cors());
-    //this.app.use(express.static('public'))
+    this.app.use(express.static('public'))
     this.app.use(
       fileupload({
         useTempFiles: true,
